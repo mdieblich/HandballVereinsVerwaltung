e61fcad6b22e50cfff08c3ca7aae4ebf347ae869
Martin Dieblich <martin.dieblich@gmx.de>
2023-02-27 11:55:51 +0100

    config-sample erstellt

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # HandballVereinsVerwaltung
 
+## Vorraussetzungen
+PHP 8.2
+Eine MySQL- oder MariaDB-Datenbank muss vorhanden sein
+Ein Email-Server mit Email-Konto.
+
+## Einrichtung
+Die Datei config-sample.php umbenennen zu config.php und dort drin alle Parameter anpassen.
+
 
 Tests can be startet with:
   phpunit tests
---
+++ config-sample.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Technische Grundeinstellungen für die Vereinsverwaltung
+ */
+
+/**
+ * Datenbank-Parameter
+ */
+define( 'DB_NAME', '' );
+define( 'DB_USER', '' );
+define( 'DB_PASSWORD', '' );
+define( 'DB_HOST', '' );
+
+/**
+ * Email-Parameter
+ */
+define( 'SMTP_SERVER', '' );
+define( 'BOT_EMAIL', '' );
+define( 'BOT_PASSWORT', '' );
+
+?>
