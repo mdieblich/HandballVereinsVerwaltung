7047cdb3e514ee249285b36ce7910e8fd17c683f
Martin Dieblich <martin.dieblich@gmx.de>
2023-02-27 01:34:22 +0100

    Erstellen aller relevanten Tabellen

--- seed/DBScheme.php
+++ seed/DBScheme.php
@@ -1,7 +1,16 @@
 <?php
 
 require_once __DIR__."/../dao/DBHandle.php";
+
 require_once __DIR__."/../dao/VereinDAO.php";
+require_once __DIR__."/../dao/MannschaftDAO.php";
+
+require_once __DIR__."/../dao/spielbetrieb/MeisterschaftDAO.php";
+require_once __DIR__."/../dao/spielbetrieb/LigaDAO.php";
+require_once __DIR__."/../dao/spielbetrieb/MannschaftsMeldungDAO.php";
+require_once __DIR__."/../dao/spielbetrieb/SpielDAO.php";
+
+require_once __DIR__."/../dao/dienst/DienstDAO.php";
 
 class DBScheme {
     private $dbHandle;
@@ -16,7 +25,20 @@ class DBScheme {
     }
 
     public function seed(): void {
-        $sql = VereinDAO::tableCreation($this->dbHandle);
+        $this->create("Verein");
+        $this->create("Mannschaft");
+
+        $this->create("Meisterschaft");
+        $this->create("Liga");
+        $this->create("MannschaftsMeldung");
+        $this->create("Spiel");
+
+        $this->create("Dienst");
+    }
+
+    private function create(string $entityName){
+        $daoName = $entityName."DAO";
+        $sql = $daoName::tableCreation($this->dbHandle);
         $this->dbHandle->query($sql);
     }
 }
