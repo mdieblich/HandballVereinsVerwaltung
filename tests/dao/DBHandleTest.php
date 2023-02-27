b26237ec17c3a0b06db2eb56021f135d098cf89d
Martin Dieblich <martin.dieblich@gmx.de>
2023-02-27 01:02:04 +0100

    reptititive Aufgaben in setUp()

--- tests/dao/DBHandleTest.php
+++ tests/dao/DBHandleTest.php
@@ -5,59 +5,56 @@ require_once __DIR__."/../../dao/DBhandle.php";
 
 final class DBHandleTest extends TestCase
 {
-    private function createTestDBConnection(): DBHandle {
-        return new DBHandle(
+    private DBHandle $dbHandle;
+
+    protected function setUp(): void {
+        $this->dbHandle = new DBHandle(
             "localhost",
             "root",
             "",
             "testdb"
         );
+        $success = $this->dbHandle->query("DROP TABLE IF EXISTS A");
+        $this->assertTrue($success);
     }
 
+
     public function testCanCreateConnection(): void {
-        $dbHandle = $this->createTestDBConnection();
-        $this->assertNotNull($dbHandle);
+        $this->assertNotNull($this->dbHandle);
     }
     
     public function testCanQuery(): void {
-        $dbHandle = $this->createTestDBConnection();
-        $success = $dbHandle->query("SELECT CURRENT_TIMESTAMP");
+        $success = $this->dbHandle->query("SELECT CURRENT_TIMESTAMP");
         $this->assertTrue($success);
     }
 
     public function testGet_var(): void {
-        $dbHandle = $this->createTestDBConnection();
-        $success = $dbHandle->query("DROP TABLE IF EXISTS A");
-        $success = $dbHandle->query("CREATE TABLE A (b int)");
+        $success = $this->dbHandle->query("CREATE TABLE A (b int)");
         $this->assertTrue($success);
-        $success = $dbHandle->query("INSERT INTO A (b) VALUES (3)");
+        $success = $this->dbHandle->query("INSERT INTO A (b) VALUES (3)");
         $this->assertTrue($success);
 
-        $value = $dbHandle->get_var("SELECT b FROM A");
+        $value = $this->dbHandle->get_var("SELECT b FROM A");
         $this->assertEquals("3", $value);
     }
 
     public function testGet_row_as_array(): void {
-        $dbHandle = $this->createTestDBConnection();
-        $success = $dbHandle->query("DROP TABLE IF EXISTS A");
-        $success = $dbHandle->query("CREATE TABLE A (b int, c VARCHAR(25))");
+        $success = $this->dbHandle->query("CREATE TABLE A (b int, c VARCHAR(25))");
         $this->assertTrue($success);
-        $success = $dbHandle->query("INSERT INTO A (b,c) VALUES (9,'testGet_row_as_array')");
+        $success = $this->dbHandle->query("INSERT INTO A (b,c) VALUES (9,'testGet_row_as_array')");
         $this->assertTrue($success);
 
-        $value = $dbHandle->get_row_as_array("SELECT * FROM A");
+        $value = $this->dbHandle->get_row_as_array("SELECT * FROM A");
         $expectedArray = array('b' => 9, 'c' => 'testGet_row_as_array');
         $this->assertEquals($expectedArray, $value);
     }
     public function testGet_results_as_array(): void {
-        $dbHandle = $this->createTestDBConnection();
-        $success = $dbHandle->query("DROP TABLE IF EXISTS A");
-        $success = $dbHandle->query("CREATE TABLE A (b int, c VARCHAR(25))");
+        $success = $this->dbHandle->query("CREATE TABLE A (b int, c VARCHAR(25))");
         $this->assertTrue($success);
-        $success = $dbHandle->query("INSERT INTO A (b,c) VALUES (2,'testGet_results_as_array0'), (4,'testGet_results_as_array1')");
+        $success = $this->dbHandle->query("INSERT INTO A (b,c) VALUES (2,'testGet_results_as_array0'), (4,'testGet_results_as_array1')");
         $this->assertTrue($success);
 
-        $value = $dbHandle->get_results_as_array("SELECT * FROM A");
+        $value = $this->dbHandle->get_results_as_array("SELECT * FROM A");
         $expectedArray = array();
         $expectedArray[] = array('b' => 2, 'c' => 'testGet_results_as_array0');
         $expectedArray[] = array('b' => 4, 'c' => 'testGet_results_as_array1');
@@ -65,60 +62,52 @@ final class DBHandleTest extends TestCase
     }
     
     public function testCanInsertBooleanTrue(): void {
-        $dbHandle = $this->createTestDBConnection();
-        $success = $dbHandle->query("DROP TABLE IF EXISTS A");
-        $success = $dbHandle->query("CREATE TABLE A (b TINYINT)");
+        $success = $this->dbHandle->query("CREATE TABLE A (b TINYINT)");
         $this->assertTrue($success);
 
         // act
         $array_to_insert = array('b' => true);
-        $dbHandle->insert("A", $array_to_insert);
+        $this->dbHandle->insert("A", $array_to_insert);
 
         // assert
-        $value = $dbHandle->get_var("SELECT b FROM A");
+        $value = $this->dbHandle->get_var("SELECT b FROM A");
         $this->assertEquals(1, $value);
     }
     public function testCanInsertBooleanFalse(): void {
-        $dbHandle = $this->createTestDBConnection();
-        $success = $dbHandle->query("DROP TABLE IF EXISTS A");
-        $success = $dbHandle->query("CREATE TABLE A (b TINYINT)");
+        $success = $this->dbHandle->query("CREATE TABLE A (b TINYINT)");
         $this->assertTrue($success);
 
         // act
         $array_to_insert = array('b' => false);
-        $dbHandle->insert("A", $array_to_insert);
+        $this->dbHandle->insert("A", $array_to_insert);
 
         // assert
-        $value = $dbHandle->get_var("SELECT b FROM A");
+        $value = $this->dbHandle->get_var("SELECT b FROM A");
         $this->assertEquals(0, $value);
     }
     public function testCanInsertIntegers(): void {
-        $dbHandle = $this->createTestDBConnection();
-        $success = $dbHandle->query("DROP TABLE IF EXISTS A");
-        $success = $dbHandle->query("CREATE TABLE A (b int)");
+        $success = $this->dbHandle->query("CREATE TABLE A (b int)");
         $this->assertTrue($success);
 
         // act
         $array_to_insert = array('b' => 16);
-        $dbHandle->insert("A", $array_to_insert);
+        $this->dbHandle->insert("A", $array_to_insert);
 
         // assert
-        $value = $dbHandle->get_var("SELECT b FROM A");
+        $value = $this->dbHandle->get_var("SELECT b FROM A");
         $this->assertEquals(16, $value);
     }
 
     public function testCanInsertStrings(): void {
-        $dbHandle = $this->createTestDBConnection();
-        $success = $dbHandle->query("DROP TABLE IF EXISTS A");
-        $success = $dbHandle->query("CREATE TABLE A (b VARCHAR(1024))");
+        $success = $this->dbHandle->query("CREATE TABLE A (b VARCHAR(1024))");
         $this->assertTrue($success);
 
         // act
         $array_to_insert = array('b' => 'testCanInsertStrings');
-        $dbHandle->insert("A", $array_to_insert);
+        $this->dbHandle->insert("A", $array_to_insert);
 
         // assert
-        $value = $dbHandle->get_var("SELECT b FROM A");
+        $value = $this->dbHandle->get_var("SELECT b FROM A");
         $this->assertEquals('testCanInsertStrings', $value);
     }
 
@@ -127,32 +116,27 @@ final class DBHandleTest extends TestCase
         $this->expectException(InvalidArgumentException::class);
 
         // arrange
-        $dbHandle = $this->createTestDBConnection();
-        $success = $dbHandle->query("DROP TABLE IF EXISTS A");
-        $success = $dbHandle->query("CREATE TABLE A (b int)");
+        $success = $this->dbHandle->query("CREATE TABLE A (b int)");
         $this->assertTrue($success);
 
         // act
         $exampleObject = new ExampleClass();
         $exampleObject->someValue = 99;
         $array_to_insert = array('b' => $exampleObject);
-        $dbHandle->insert("A", $array_to_insert);
+        $this->dbHandle->insert("A", $array_to_insert);
     }
 
     public function testGetInsertedId(): void {
-        $dbHandle = $this->createTestDBConnection();
-        $success = $dbHandle->query("DROP TABLE IF EXISTS A");
-        $this->assertTrue($success);
-        $success = $dbHandle->query("CREATE TABLE A (id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY (id), b int)");
+        $success = $this->dbHandle->query("CREATE TABLE A (id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY (id), b int)");
         $this->assertTrue($success);
 
         // act
         $array_to_insert = array('b' => 16);
-        $dbHandle->insert("A", $array_to_insert);
-        $firstInsertedID = $dbHandle->insert_id;
+        $this->dbHandle->insert("A", $array_to_insert);
+        $firstInsertedID = $this->dbHandle->insert_id;
         $array_to_insert = array('b' => 17);
-        $dbHandle->insert("A", $array_to_insert);
-        $secondInsertedID = $dbHandle->insert_id;
+        $this->dbHandle->insert("A", $array_to_insert);
+        $secondInsertedID = $this->dbHandle->insert_id;
 
         // assert
         $this->assertGreaterThan($firstInsertedID, $secondInsertedID);
@@ -160,46 +144,40 @@ final class DBHandleTest extends TestCase
 
     public function testUpdateWithOneKey(): void {
         // arrange
-        $dbHandle = $this->createTestDBConnection();
-        $success = $dbHandle->query("DROP TABLE IF EXISTS A");
-        $this->assertTrue($success);
-        $success = $dbHandle->query("CREATE TABLE A (b VARCHAR(50), c INT)");
+        $success = $this->dbHandle->query("CREATE TABLE A (b VARCHAR(50), c INT)");
         $this->assertTrue($success);
         
-        $success = $dbHandle->query("INSERT INTO A (b,c) VALUES ('please change',2),('do not change',4)");
+        $success = $this->dbHandle->query("INSERT INTO A (b,c) VALUES ('please change',2),('do not change',4)");
         $this->assertTrue($success);
         
         // act
-        $dbHandle->update("A", array('b' => 'changed'), array('c' => 2));
+        $this->dbHandle->update("A", array('b' => 'changed'), array('c' => 2));
 
         // assert
-        $unchangedValueForB = $dbHandle->get_var("SELECT b FROM A where c=4");
+        $unchangedValueForB = $this->dbHandle->get_var("SELECT b FROM A where c=4");
         $this->assertEquals('do not change', $unchangedValueForB);
-        $newValueForB = $dbHandle->get_var("SELECT b FROM A where c=2");
+        $newValueForB = $this->dbHandle->get_var("SELECT b FROM A where c=2");
         $this->assertEquals('changed', $newValueForB);
 
     }
 
     public function testUpdateWithMultipleKeys(): void {
         // arrange
-        $dbHandle = $this->createTestDBConnection();
-        $success = $dbHandle->query("DROP TABLE IF EXISTS A");
-        $this->assertTrue($success);
-        $success = $dbHandle->query("CREATE TABLE A (b VARCHAR(50), c INT, d INT)");
+        $success = $this->dbHandle->query("CREATE TABLE A (b VARCHAR(50), c INT, d INT)");
         $this->assertTrue($success);
         
-        $success = $dbHandle->query("INSERT INTO A (b,c,d) VALUES ('please change',1,1),('do not change',1,2),('do not change',2,1)");
+        $success = $this->dbHandle->query("INSERT INTO A (b,c,d) VALUES ('please change',1,1),('do not change',1,2),('do not change',2,1)");
         $this->assertTrue($success);
         
         // act
-        $dbHandle->update("A", array('b' => 'changed'), array('c' => 1, 'd' => 1));
+        $this->dbHandle->update("A", array('b' => 'changed'), array('c' => 1, 'd' => 1));
 
         // assert
-        $unchangedValue0 = $dbHandle->get_var("SELECT b FROM A where c=1 AND d=2");
+        $unchangedValue0 = $this->dbHandle->get_var("SELECT b FROM A where c=1 AND d=2");
         $this->assertEquals('do not change', $unchangedValue0);
-        $unchangedValue1 = $dbHandle->get_var("SELECT b FROM A where c=2 AND d=1");
+        $unchangedValue1 = $this->dbHandle->get_var("SELECT b FROM A where c=2 AND d=1");
         $this->assertEquals('do not change', $unchangedValue1);
-        $changedValue = $dbHandle->get_var("SELECT b FROM A where c=1 AND d=1");
+        $changedValue = $this->dbHandle->get_var("SELECT b FROM A where c=1 AND d=1");
         $this->assertEquals('changed', $changedValue);
 
     }
