b26f8554ed3fb46bb9457e1b1f33cbf734c6ca51
Martin Dieblich <martin.dieblich@gmx.de>
2023-02-27 17:08:52 +0100

    config-sample um Vereinsname- und nuliga-ID erweitert

--- config-sample.php
+++ config-sample.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Technische Grundeinstellungen für die Vereinsverwaltung
+ * Grundeinstellungen für die Vereinsverwaltung
  */
 
 /**
@@ -18,4 +18,13 @@ define( 'SMTP_SERVER', '' );
 define( 'BOT_EMAIL', '' );
 define( 'BOT_PASSWORT', '' );
 
+/**
+ * Vereinseinstellungen
+ * die Nuliga-ID kann man aus dem Parameter club in der URL entnehmen, 
+ *   Bsp. für den Turnerkreis Nippes:
+ *   https://hvmittelrhein-handball.liga.nu/cgi-bin/WebObjects/nuLigaHBDE.woa/wa/clubInfoDisplay?club=74851
+ *   >>74851<< is die nuLiga-ID
+ */
+define( 'VEREIN_NAME', '' );
+define( 'VEREIN_NULIGA_ID', '' );
 ?>
