e27e74eaf0fc9cd0a70ff3316710d853635acc8a
Martin Dieblich <martin.dieblich@gmx.de>
2023-02-28 14:33:36 +0100

    require später

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 
-require_once __DIR__."/dao/DBhandle.php";
-require_once __DIR__."/seed/DBSeeder.php";
-require_once __DIR__."/seed/DBScheme.php";
-
 if((include_once __DIR__.'/config.php') != TRUE){
     echo "Die config.php muss noch erstellt werden. Bitte sehen Sie dazu in die config-sample.php.";
     exit;
 }
+
+require_once __DIR__."/dao/DBhandle.php";
+require_once __DIR__."/seed/DBSeeder.php";
+require_once __DIR__."/seed/DBScheme.php";
 
 $dbHandle = DBHandle::createFromConfig();
 $dbSeeder = new DBSeeder($dbHandle);
