8553a2891fd41187a92f6bd565ad791b53c3e9f0
Martin Dieblich <martin.dieblich@gmx.de>
2023-02-28 01:22:04 +0100

    index.php: Checkt auf Config & Seeden

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once __DIR__."/dao/DBhandle.php";
+require_once __DIR__."/seed/DBSeeder.php";
+require_once __DIR__."/seed/DBScheme.php";
+
+if((include_once __DIR__.'/config.php') != TRUE){
+    echo "Die config.php muss noch erstellt werden. Bitte sehen Sie dazu in die config-sample.php.";
+    exit;
+}
+
+$dbHandle = DBHandle::createFromConfig();
+$dbSeeder = new DBSeeder($dbHandle);
+if($dbSeeder->needsSeeding()){
+    $dbScheme = new DBScheme($dbHandle);
+    if($dbScheme->needsSeeding()){
+        echo "Schema wird initialisiert<br>\n";
+        $dbScheme->seed();
+    }
+    echo "Datenbankinhalt wird initialisiert<br>\n";
+    $dbSeeder->seed();
+}
+?>
